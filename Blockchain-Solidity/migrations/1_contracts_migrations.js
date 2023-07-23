a3daeef1b49153d235f0e983a505cc4cf07db815
Pavithra V <v.pavithra2011@gmail.com>
2023-07-23 16:06:50 +0530

    Delete Blockchain-Solidity directory

--- Blockchain-Solidity/migrations/1_contracts_migrations.js
+++
@@ -1,6 +0,0 @@
-//const UserAuth = artifacts.require("./UserAuthentication.sol");
-const Pright = artifacts.require("./PrescribeRight.sol");
-
-module.exports = function(deployer) {
-    deployer.deploy(Pright);
-};
